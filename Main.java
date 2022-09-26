6ce1e77da275ed2a2be2f0b51a12c747fe08cdd0
xdTobs <70943428+xdTobs@users.noreply.github.com>
2022-09-26 16:07:48 +0200

    added Main class

--- Main.java
+++ Main.java
@@ -1,3 +1,12 @@
+import java.awt.*;
+/*
+import gui_fields.GUI_Board;
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Player;
+import gui_fields.GUI_Street;
+import gui_main.GUI;
+*/
+
 public class Main {
     public static void main(String[] args) {
 
